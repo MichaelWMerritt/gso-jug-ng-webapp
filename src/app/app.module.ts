978735e94e60ac8581ce24dc7552f070d5fc1025
subhash-koganti <subhash.koganti@gmail.com>
2019-11-13 21:55:13 -0500

    added main-page-carousel component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MainPageCarouselComponent } from './main-page/main-page-carousel/main-page-carousel.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FooterComponent } from './footer/footer.component';
     MainPageComponent,
     NavBarComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MainPageCarouselComponent
   ],
   imports: [
     BrowserModule,
